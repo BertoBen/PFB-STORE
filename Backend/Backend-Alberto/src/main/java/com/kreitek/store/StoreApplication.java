233a3e87bc3facc5a0e73afc8b63c712d35d880f
Alberto Benitez <alberbenig@hotmail.com>
2023-06-11 18:34:09 +0200

    solucion errores

--- Backend/Backend-Alberto/src/main/java/com/kreitek/store/StoreApplication.java
+++ Backend/Backend-Alberto/src/main/java/com/kreitek/store/StoreApplication.java
@@ -2,12 +2,30 @@ package com.kreitek.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreApplication.class, args);
+	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.addAllowedOrigin("http://localhost:4200");
+		configuration.addAllowedMethod("*");
+		configuration.addAllowedHeader("*");
+		configuration.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+
+		return new CorsFilter(source);
 	}
 
 }
